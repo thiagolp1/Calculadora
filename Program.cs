09c2e12e15433c3c2908c952797e1ec2347a11b5
thiagolp1 <100002021+thiagolp1@users.noreply.github.com>
2022-04-29 20:23:49 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿string opcaoDesejada;
+
+Console.Clear();
+
+Console.WriteLine("--- Calculadora 4 operações ---\n");
+
+Console.WriteLine("[+] Soma");
+Console.WriteLine("[-] Subtração");
+Console.WriteLine("[*] Multiplicação");
+Console.WriteLine("[/] Divisão");
+
+Console.Write("\nSelecione a operação desejada: ");
+
+opcaoDesejada = Console.ReadLine()!;
+
+switch (opcaoDesejada)
+{
+    case "+":
+        Console.WriteLine("Você selecionou soma");
+        break;
+    case "-":
+        Console.WriteLine("Você selecionou subtração");
+        break;
+    case "*":
+        double a, b, resultado;
+
+        Console.WriteLine("--- Multiplicação A*B ---\n");
+        Console.WriteLine("Digite os valores.");
+
+        Console.Write("A = ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("B = ");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        resultado = a * b;
+
+        Console.WriteLine($"{a} multiplicado por {b} é {resultado}");
+
+        Console.WriteLine("\nPressione uma tecla para continuar...");
+        Console.ReadKey();
+
+        break;
+    case "/":
+        Console.WriteLine("Você selecionou divisão");
+        break;
+    default:
+        Console.WriteLine("Opção inválida.");
+        break;
+}
+
+Console.WriteLine("\nObrigado por utilizar nosso programa.");
---
+++ README.md
@@ -0,0 +1,2 @@
+# calculadora-2022
+Uma calculadora com as 4 operações aritméticas em C#
