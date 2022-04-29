466442d3238d8e40a3c8de4114eb5d89f110aa64
Thiago <Thiagolopespereiralp1@gmail.com>
2022-04-29 20:41:48 -0300

    termino da soma

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿string opcaoDesejada;
+double a, b, resultado;
 
 Console.Clear();
 
@@ -16,14 +17,27 @@ opcaoDesejada = Console.ReadLine()!;
 switch (opcaoDesejada)
 {
     case "+":
-        Console.WriteLine("Você selecionou soma");
+        
+        Console.WriteLine("soma A + B \n");
+        Console.WriteLine("Digite os valores");
+
+        Console.Write("A =");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("B =");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        resultado = a + b;
+        Console.WriteLine($"{a} + {b} é {resultado}");
+        
+        Console.WriteLine("\nPressione uma tecla para continuar...");
+        Console.ReadKey();
         break;
     case "-":
         Console.WriteLine("Você selecionou subtração");
         break;
     case "*":
-        double a, b, resultado;
-
+        
         Console.WriteLine("--- Multiplicação A*B ---\n");
         Console.WriteLine("Digite os valores.");
 
